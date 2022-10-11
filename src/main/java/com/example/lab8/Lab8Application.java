17014003b09bbdfbcb28cd192e597ce96aeed72a
baspop01 <63070115@it.kmitl.ac.th>
2022-10-11 09:42:18 +0700

    Initial commit

---
+++ src/main/java/com/example/lab8/Lab8Application.java
@@ -0,0 +1,15 @@
+package com.example.lab8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Lab8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab8Application.class, args);
+    }
+
+}
